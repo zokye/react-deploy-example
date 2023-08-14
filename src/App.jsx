82d979d3658344d66bbc55c48b65b551d9bf07fb
zokye <zokye@outlook.com>
2023-08-14 13:49:59 -0600

    mi primer commit de app react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+
+import { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10"> 
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+import { task as data } from "../data/task";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider value={{ tasks, deleteTask, createTask }}>
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
